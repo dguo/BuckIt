3b56edb26b5917776e2f64d4d907e1715ad1012c
Danny Guo <dyguo@princeton.edu>
2013-04-12 13:07:05 -0400

    added buckitapp in Django and enabled admin

--- BuckIt/BuckItList/views.py
+++
@@ -1,8 +0,0 @@
-# Create your views here.
-from django.shortcuts import render_to_response
-from django.template import RequestContext
-
-def list(request, title):
-	list_name = title.replace('_',' ')
-	return render_to_response('list.html',
-		{'list_name': list_name,})
