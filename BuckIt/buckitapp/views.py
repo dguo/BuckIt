a5259dc3712c2c939b390f8b58879b97dcde0552
Molly Nacey <mnacey@princeton.edu>
2013-04-16 19:27:27 -0400

    remove extra apps

--- BuckIt/buckitapp/views.py
+++ BuckIt/buckitapp/views.py
@@ -1,1 +1,12 @@
 # Create your views here.
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+def home(request):
+	return render_to_response('home.html')
+
+def login(request):
+	return render_to_response('login.html', context_instance=RequestContext(request))
+
+def profile(request, userid):
+	return render_to_response('profile.html')# Create your views here.
--- BuckIt/urls.py
+++ BuckIt/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('BuckIt.home.views',
+urlpatterns = patterns('BuckIt.buckitapp.views',
     # Examples:
     # url(r'^$', 'BuckIt.views.home', name='home'),
     # url(r'^BuckIt/', include('BuckIt.foo.urls')),
