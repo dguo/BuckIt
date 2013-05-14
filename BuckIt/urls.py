cba7e5b615289cc9350cc09f208f87bf799b5056
Molly Nacey <mnacey@princeton.edu>
2013-05-13 21:42:22 -0400

    fix typos

--- BuckIt/urls.py
+++ BuckIt/urls.py
@@ -15,7 +15,7 @@ urlpatterns = patterns('BuckIt.buckitapp.views',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'login'),
-    url(r'^login/(?P<errorcode>\w+)/$', 'login'),
+    url(r'^login/(?P<errorcode>\d+)/$', 'login'),
     url(r'^login/$', 'login'),
     url(r'^home/$', 'home'),
     url(r'^profile/(?P<userid>\w+)/$', 'profile'),
